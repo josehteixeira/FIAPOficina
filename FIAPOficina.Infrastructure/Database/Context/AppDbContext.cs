adb7c86fb1ed1de10ff7f09643378114efa4e99b
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-09-28 18:08:01 -0300

    Criação de DbContext

---
+++ FIAPOficina.Infrastructure/Database/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FIAPOficina.Infrastructure.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FIAPOficina.Infrastructure.Database.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Aplica automaticamente todas as configurações de IEntityTypeConfiguration
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FIAPOficina.Infrastructure/Database/Context/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FIAPOficina.Infrastructure.Database.Context
+{
+    internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Connection string para design time (Postgres)
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=FIAPOficina;Username=root;Password=root");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
