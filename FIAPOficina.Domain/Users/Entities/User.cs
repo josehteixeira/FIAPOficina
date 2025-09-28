d203d3bc650bbc2159f25356f8cdcf60d38635a4
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-09-28 18:08:22 -0300

    Criação de domínio de Users

---
+++ FIAPOficina.Domain/Users/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿namespace FIAPOficina.Domain.Users.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string UserName { get; private set; }
+
+        public User(string name, string userName, Guid? id = null)
+        {
+            CheckRequiredFields(name, userName);
+
+            if (id.HasValue) Id = id.Value;
+
+            Name = name;
+            UserName = userName;
+        }
+
+        public User(User user, Guid? id = null)
+        {
+            CheckRequiredFields(user.Name, user.UserName);
+
+            if (id.HasValue) Id = id.Value;
+
+            Name = user.Name;
+            UserName = user.UserName;
+        }
+
+        private void CheckRequiredFields(string name, string userName)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("The user must have a name.", nameof(name));
+            if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentException("The user must have a user name.", nameof(userName));
+        }
+    }
+}
---
+++ FIAPOficina.Domain/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using FIAPOficina.Domain.Users.Entities;
+
+namespace FIAPOficina.Domain.Users.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> AddAsync(User user, string passwordHash);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
