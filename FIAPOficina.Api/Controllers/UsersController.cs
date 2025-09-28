6791debe46d3e583d6de3f45052fe786c87bb79a
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-09-28 18:11:22 -0300

    Controller de Users

---
+++ FIAPOficina.Api/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using FIAPOficina.Api.Helpers;
+using FIAPOficina.Application.Users.Commands.CreateUser;
+using FIAPOficina.Application.Users.Services;
+using FIAPOficina.Domain.Users.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIAPOficina.Api.Controllers
+{
+    [ApiController]
+    [Route(RoutesHelper.Users.Controller)]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost(RoutesHelper.Users.Create)]
+        [ProducesResponseType(typeof(User), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes("application/json")]
+        public async Task<ActionResult<User>> Create([FromBody] CreateUserCommand command)
+        {
+            var user = await _usersService.AddAsync(new(
+                Name: command.Name,
+                UserName: command.UserName,
+                Password: command.Password
+            ));
+
+            return Created((Uri)null!, user);
+        }
+    }
+}
---
+++ FIAPOficina.Api/Helpers/RoutesHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace FIAPOficina.Api.Helpers
+{
+    public static class RoutesHelper
+    {
+        public static class Users
+        {
+            public const string Controller = "/api/v1/users";
+            public const string Create = "/api/v1/users";
+            public const string Update = "/api/v1/users/{id}";
+            public const string Delete = "/api/v1/users";
+        }
+    }
+}
