684dc1fbeeffa273a88d37ffb2087b5ba75c0db1
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-14 22:33:21 -0300

    Inclusão dos novos comandos de User no Service

--- FIAPOficina.Application/Users/Services/IUsersService.cs
+++ FIAPOficina.Application/Users/Services/IUsersService.cs
@@ -1,4 +1,6 @@
 ﻿using FIAPOficina.Application.Users.Commands.CreateUser;
+using FIAPOficina.Application.Users.Commands.DeteleUser;
+using FIAPOficina.Application.Users.Commands.UpdateUser;
 using FIAPOficina.Domain.Users.Entities;
 
 namespace FIAPOficina.Application.Users.Services
@@ -6,5 +8,7 @@ namespace FIAPOficina.Application.Users.Services
     public interface IUsersService
     {
         public Task<User> AddAsync(CreateUserCommand command);
+        public Task<User> UpdateAsync(UpdateUserCommand command);
+        public Task DeleteAsync(DeleteUserCommand command);
     }
 }
--- FIAPOficina.Application/Users/Services/UsersService.cs
+++ FIAPOficina.Application/Users/Services/UsersService.cs
@@ -1,5 +1,7 @@
 ﻿using FIAPOficina.Application.Common.Security;
 using FIAPOficina.Application.Users.Commands.CreateUser;
+using FIAPOficina.Application.Users.Commands.DeteleUser;
+using FIAPOficina.Application.Users.Commands.UpdateUser;
 using FIAPOficina.Domain.Users.Entities;
 using FIAPOficina.Domain.Users.Repositories;
 
@@ -8,15 +10,29 @@ namespace FIAPOficina.Application.Users.Services
     public class UsersService : IUsersService
     {
         private readonly CreateUserCommandHandler _createHandler;
+        private readonly UpdateUserCommandHandler _updateHandler;
+        private readonly DeleteUserCommandHandler _deleteHandler;
 
         public UsersService(IUserRepository repository, IPasswordHasherService passwordHasher)
         {
             _createHandler = new CreateUserCommandHandler(repository, passwordHasher);
+            _updateHandler = new UpdateUserCommandHandler(repository);
+            _deleteHandler = new DeleteUserCommandHandler(repository);
         }
 
         public async Task<User> AddAsync(CreateUserCommand command)
         {
             return await _createHandler.Handle(command);
+        }
+
+        public async Task<User> UpdateAsync(UpdateUserCommand command)
+        {
+            return await _updateHandler.Handle(command);
+        }
+
+        public async Task DeleteAsync(DeleteUserCommand command)
+        {
+            await _deleteHandler.Handle(command);
         }
     }
 }
