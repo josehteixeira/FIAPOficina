12dfc21eea00e972f1bf7d6e46f8cd414ad63dd2
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-14 22:33:03 -0300

    Comando para atualizar User

---
+++ FIAPOficina.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using FIAPOficina.Domain.Users.Entities;
+using FIAPOficina.Domain.Users.Repositories;
+
+namespace FIAPOficina.Application.Users.Commands.UpdateUser
+{
+    internal class UpdateUserCommandHandler
+    {
+        private readonly IUserRepository _repository;
+
+        public UpdateUserCommandHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<User> Handle(UpdateUserCommand command)
+        {
+            var user = new User(command.Name, command.UserName, command.Id);
+
+            await _repository.UpdateAsync(user);
+
+            return user;
+        }
+    }
+}
