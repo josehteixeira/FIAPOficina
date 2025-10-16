973d5f6409fc83e72649ce1a1a8069d4679e4038
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-16 01:33:22 -0300

    Modelos de request/response de Client

---
+++ FIAPOficina.Api/Models/Clients/Requests/CreateClientRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIAPOficina.Api.Models.Clients.Requests
+{
+    public record CreateClientRequest(
+        [Required] string Name,
+        [Required] string Identifier,
+        [Required] string Phone,
+        [Required] string Email,
+        [Required] string Address
+    );
+}
---
+++ FIAPOficina.Api/Models/Clients/Responses/ClientResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FIAPOficina.Api.Models.Clients.Responses
+{
+    public record ClientResponse(
+        Guid Id,
+        string Name,
+        string Identifier,
+        string Phone,
+        string Email,
+        string Address
+    );
+}
