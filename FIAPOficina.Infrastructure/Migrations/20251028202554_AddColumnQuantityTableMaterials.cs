847235a6d5544b3e94945299d395d1d741d92940
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-28 18:09:12 -0300

    Migration alterando Materials e Services

---
+++ FIAPOficina.Infrastructure/Migrations/20251028202554_AddColumnQuantityTableMaterials.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FIAPOficina.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnQuantityTableMaterials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Value",
+                table: "Services",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Materials",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Materials");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Value",
+                table: "Services",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
