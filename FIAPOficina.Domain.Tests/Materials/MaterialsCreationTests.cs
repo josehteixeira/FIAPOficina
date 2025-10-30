98646fbc6633313b0a56621383a1dafe56750cdc
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-30 00:41:54 -0300

    Correção do nome de MaterialsCreationTests

--- FIAPOficina.Domain.Tests/Materials/MaterialsCreationTests.cs
+++ FIAPOficina.Domain.Tests/Materials/MaterialsCreationTests.cs
@@ -1,24 +1,19 @@
 ﻿using FIAPOficina.Domain.Materials.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FIAPOficina.Domain.Tests.Materials
 {
-    public class MaterialsCretionTests
+    public class MaterialsCreationTests
     {
         [Fact]
         public void Should_Create_Valid_Material()
         {
             Guid id = Guid.NewGuid();
 
-            Material material = new Material("Pastilha de Freio","Pastilha de freio muito resistente","Freia Bem",36.00m, id,4);
+            Material material = new Material("Pastilha de Freio","Pastilha de freio muito resistente","Freia Bem",36.00m, 4, id);
             Assert.NotNull(material);
             Assert.Equal("Pastilha de Freio", material.Name);
             Assert.Equal("Pastilha de freio muito resistente", material.Description);
-            Assert.Equal("Freia Bem",material.Brand);
+            Assert.Equal("Freia Bem", material.Brand);
             Assert.Equal(36.00m, material.Value);
             Assert.Equal(4, material.Quantity);
             Assert.Equal(id, material.Id);
