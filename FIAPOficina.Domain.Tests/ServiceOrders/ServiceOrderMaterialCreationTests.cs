d10adcaab85116c4df79aad6bb3436c6f90d706b
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-10-30 00:42:22 -0300

    Ajustes de indentação e remoção de código desnecessário

--- FIAPOficina.Domain.Tests/ServiceOrders/ServiceOrderMaterialCreationTests.cs
+++ FIAPOficina.Domain.Tests/ServiceOrders/ServiceOrderMaterialCreationTests.cs
@@ -1,9 +1,4 @@
 ﻿using FIAPOficina.Domain.ServiceOrders.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FIAPOficina.Domain.Tests.ServiceOrders
 {
@@ -15,10 +10,10 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
             Guid materialId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
 
-            ServiceOrderMaterial serviceOrderMaterial = new ServiceOrderMaterial(materialId, serviceOrderID,4,36.00m);
+            ServiceOrderMaterial serviceOrderMaterial = new ServiceOrderMaterial(materialId, serviceOrderID, 4, 36.00m, Guid.NewGuid());
             Assert.NotNull(serviceOrderMaterial);
-            Assert.Equal(4,serviceOrderMaterial.Quantity);
-            Assert.Equal(36.00m,serviceOrderMaterial.UnitValue);
+            Assert.Equal(4, serviceOrderMaterial.Quantity);
+            Assert.Equal(36.00m, serviceOrderMaterial.UnitValue);
             Assert.Equal(144.00m, serviceOrderMaterial.TotalValue);
             Assert.Equal(materialId, serviceOrderMaterial.MaterialId);
             Assert.Equal(serviceOrderID, serviceOrderMaterial.ServiceOrderId);
@@ -29,14 +24,14 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
         {
             Guid materialId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
-            Assert.Throws<ArgumentOutOfRangeException>(()=>new ServiceOrderMaterial(materialId, serviceOrderID, -1, 36.00m));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderMaterial(materialId, serviceOrderID, -1, 36.00m, Guid.NewGuid()));
         }
         [Fact]
         public void Should_Create_Valid_ServiceOrderMaterial_Throw_ArgumentException_UnitValue()
         {
             Guid materialId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
-            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderMaterial(materialId, serviceOrderID, 4, -36.00m));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderMaterial(materialId, serviceOrderID, 4, -36.00m, Guid.NewGuid()));
         }
         [Fact]
         public void Should_Create_Valid_ServiceOrderMaterial_From_Other_ServiceOrderMaterial()
@@ -46,8 +41,8 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
             Guid serviceOrderIdOriginal = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
 
-            ServiceOrderMaterial serviceOrderMaterialOriginal = new ServiceOrderMaterial(materialIdOriginal, serviceOrderIdOriginal, 4, 36.00m);
-            ServiceOrderMaterial serviceOrderMaterial = new ServiceOrderMaterial(serviceOrderMaterialOriginal,materialId, serviceOrderID );
+            ServiceOrderMaterial serviceOrderMaterialOriginal = new ServiceOrderMaterial(materialIdOriginal, serviceOrderIdOriginal, 4, 36.00m, Guid.NewGuid());
+            ServiceOrderMaterial serviceOrderMaterial = new ServiceOrderMaterial(serviceOrderMaterialOriginal, materialId, serviceOrderID);
             Assert.NotNull(serviceOrderMaterial);
             Assert.Equal(serviceOrderMaterialOriginal.Quantity, serviceOrderMaterial.Quantity);
             Assert.Equal(serviceOrderMaterialOriginal.UnitValue, serviceOrderMaterial.UnitValue);
--- FIAPOficina.Domain.Tests/ServiceOrders/ServiceOrderServiceTests.cs
+++ FIAPOficina.Domain.Tests/ServiceOrders/ServiceOrderServiceTests.cs
@@ -1,9 +1,4 @@
 ﻿using FIAPOficina.Domain.ServiceOrders.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FIAPOficina.Domain.Tests.ServiceOrders
 {
@@ -15,7 +10,7 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
             Guid serviceId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
 
-            ServiceOrderService serviceOrderService = new ServiceOrderService(serviceId, serviceOrderID, 4, 27.00m);
+            ServiceOrderService serviceOrderService = new ServiceOrderService(serviceId, serviceOrderID, 4, 27.00m, Guid.NewGuid());
             Assert.NotNull(serviceOrderService);
             Assert.Equal(4, serviceOrderService.Quantity);
             Assert.Equal(27.00m, serviceOrderService.UnitValue);
@@ -29,14 +24,14 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
         {
             Guid serviceId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
-            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderService(serviceId, serviceOrderID, -1, 36.00m));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderService(serviceId, serviceOrderID, -1, 36.00m, Guid.NewGuid()));
         }
         [Fact]
         public void Should_Create_Valid_ServiceOrderService_Throw_ArgumentException_UnitValue()
         {
             Guid materialId = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
-            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderService(materialId, serviceOrderID, 4, -36.00m));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ServiceOrderService(materialId, serviceOrderID, 4, -36.00m, Guid.NewGuid()));
         }
         [Fact]
         public void Should_Create_Valid_ServiceOrderService_From_Other_ServiceOrderService()
@@ -46,7 +41,7 @@ namespace FIAPOficina.Domain.Tests.ServiceOrders
             Guid serviceOrderIdOriginal = Guid.NewGuid();
             Guid serviceOrderID = Guid.NewGuid();
 
-            ServiceOrderService serviceOrderServiceOriginal = new ServiceOrderService(serviceIdOriginal, serviceOrderIdOriginal, 4, 36.00m);
+            ServiceOrderService serviceOrderServiceOriginal = new ServiceOrderService(serviceIdOriginal, serviceOrderIdOriginal, 4, 36.00m, Guid.NewGuid());
             ServiceOrderService serviceOrderService = new ServiceOrderService(serviceOrderServiceOriginal, serviceId, serviceOrderID);
             Assert.NotNull(serviceOrderService);
             Assert.Equal(serviceOrderServiceOriginal.Quantity, serviceOrderService.Quantity);
