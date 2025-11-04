b8f02ddbe54c638a0dfbab709a1d5586ce48f2db
José Henrique Teixeira <joseehteixeira@gmail.com>
2025-11-04 00:42:40 -0300

    Documentação da controller de usuários

--- FIAPOficina.Api/Controllers/UsersController.cs
+++ FIAPOficina.Api/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using FIAPOficina.Api.Models.Users.Responses;
 using FIAPOficina.Application.Users.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace FIAPOficina.Api.Controllers
 {
@@ -19,6 +20,10 @@ namespace FIAPOficina.Api.Controllers
         }
 
         [Authorize]
+        [SwaggerOperation(
+            Summary = "Create user.",
+            Description = "Creates a user with the provided info."
+        )]
         [HttpPost(RoutesHelper.Users.Create)]
         [ProducesResponseType(typeof(UserResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -40,6 +45,10 @@ namespace FIAPOficina.Api.Controllers
 
 
         [Authorize]
+        [SwaggerOperation(
+            Summary = "Update user.",
+            Description = "Upates a user with the provided info."
+        )]
         [HttpPut(RoutesHelper.Users.Update)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -58,6 +67,10 @@ namespace FIAPOficina.Api.Controllers
 
 
         [Authorize]
+        [SwaggerOperation(
+            Summary = "Delete user.",
+            Description = "Deletes the user with the provided ID."
+        )]
         [HttpDelete(RoutesHelper.Users.Delete)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -71,21 +84,37 @@ namespace FIAPOficina.Api.Controllers
         }
 
         [Authorize]
+        [SwaggerOperation(
+            Summary = "Get user.",
+            Description = "Returns the user that matches the provided ID."
+        )]
         [HttpGet(RoutesHelper.Users.GetSingle)]
         [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [Consumes("application/json")]
         public async Task<ActionResult<UserResponse>> GetSingle([FromRoute] Guid id)
         {
             var user = await _usersService.GetSingleAsync(new(id));
 
-            return Ok(user);
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new UserResponse(
+                    user.Id,
+                    user.Name,
+                    user.UserName
+                ));
         }
 
         [Authorize]
+        [SwaggerOperation(
+            Summary = "Get users.",
+            Description = "Returns all users."
+        )]
         [HttpGet(RoutesHelper.Users.GetAll)]
         [ProducesResponseType(typeof(UserResponse[]), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [Consumes("application/json")]
         public ActionResult<UserResponse[]> GetAll()
         {
